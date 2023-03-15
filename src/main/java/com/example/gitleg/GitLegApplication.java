b81f844e9189a6e3982fd0b4944644172f417413
Nademtis <Mathias.99.madsen@gmail.com>
2023-03-15 09:04:26 +0100

    init commit mmad

---
+++ src/main/java/com/example/gitleg/GitLegApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitleg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLegApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitLegApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitleg/GitLegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitleg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLegApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
